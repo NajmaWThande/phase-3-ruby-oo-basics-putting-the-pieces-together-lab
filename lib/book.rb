1bcb519003b4d6d4221fbb388fc21436e1002109
Najma <najmathande@gmail.com>
2023-05-19 01:53:22 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    attr_accessor :page_count, :genre, :author
+
+    def turn_page
+        puts "Flip the page... you are a fast reader!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+
+class shoe
+    def initialize(brand)
+        @brand = brand
+    end
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def cobble
+        puts "Your show is available"
+        @conditon = "new"
+    end
+end
